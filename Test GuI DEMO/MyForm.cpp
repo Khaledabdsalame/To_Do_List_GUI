d8e3c89c8f4309a812d77a77ceb209e5c4154ae2
lenovo <lenovo@DESKTOP-4M88ORO>
2024-03-27 00:33:42 +0100

    Add project files.

---
+++ Test GuI DEMO/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+#include <iostream>
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew TestGuIDEMO::MyForm);
+	
+
+}
